427283bef63d7a1dbdddeb45468397b2a28d95be
sbmraj03@gmail.com <sbmraj03@gmail.com>
2023-07-02 15:01:33 +0530

    Installing npm and other packages

---
+++ 2.Igniting Our App/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading=React.createElement("h1",{id:"title 1"},"Hello Ji!");
+    const heading2=React.createElement("h1",{id:"title 2"},"kaise ho aap sab");
+
+    const container=React.createElement("div",{id:"container"},[heading,heading2]);
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    //passing a react element inside the root
+    root.render(container);                 
