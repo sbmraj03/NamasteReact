9af62425afb08475b815b30adb8307dead8cdc5d
sbmraj03@gmail.com <sbmraj03@gmail.com>
2023-07-02 18:58:23 +0530

    jsx and functional component

---
+++ 3.Laying the foundation/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+//  Creating elements using JSX 
+const heading = (  
+  <h1 id="title" key="h3" tabIndex="" className="">
+    Namaste React
+  </h1>
+); // this is a react variable
+
+const Heading2 = () => (
+    <h1 id="title" key="h3" tabIndex="" className="">
+      Namaste React 2nd times
+    </h1>
+  ); //this is a fxnal comp
+  
+
+// React Component
+// Functional component (new) && class component (old)
+// conventionally name of the fxnl comp starts with capital letter
+
+const HeaderComponent= () => {
+    return(
+        <div>
+            {heading}
+            <h1>Namaste React using fxnl comp</h1>
+        </div> 
+    );
+};
+
+const HeaderComponent2 =()=> (
+        <div>
+            <Heading2/>
+            <h2>Namaste React using fxnl comp</h2>
+        </div> 
+    );
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+root.render(<HeaderComponent2/>);
